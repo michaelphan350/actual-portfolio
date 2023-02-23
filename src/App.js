036621dc68ec6f4bdfd6c5b7ad7cdc239cfbab7f
Michael <michaelphan350@gmail.com>
2023-02-23 15:48:32 -0700

    changed to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Home from "./pages/About"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Resume from "./pages/Resume";
@@ -12,16 +12,16 @@ function App() {
 
   
   return (
-    <Router>
-      <Routes>
+    <HashRouter>
+      <Switch>
         <Route exact path='/' exact element={<Home />} />
         <Route path='/about' element={<About/>} />
         <Route path='/projects' element={<Projects/>} />
         <Route path='/resume' element={<Resume/>} />
         <Route path='/contacts' element={<Contacts/>} />
         <Route path='/controller' element={<Controller/>} />
-    </Routes>
-    </Router>
+    </Switch>
+    </HashRouter>
   );
 }
 
